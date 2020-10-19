bdd64abbce4860cc3141efeff2f13fd5c8a87514
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-19 19:21:00 -0300

    Create Ex02.java

---
+++ ex02/Ex02.java
@@ -0,0 +1,16 @@
+package ex02;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Account conta00=new Account();
+
+
+
+        conta00.deposit(40);
+        conta00.withdraw(10);
+        conta00.accountIncome();
+        
+        conta00.situacao();
+
+    }
+}
