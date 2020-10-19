c344e4c7f65daaa352e09b248b18bad112a2a28b
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-19 18:08:34 -0300

    Create Ex00.java

---
+++ ex.00/src/main/java/ex00/Ex00.java
@@ -0,0 +1,8 @@
+package ex00;
+
+public class Ex00 {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!!");
+        System.out.println("E aí JAVA! Meu nome é Carlos :D");
+    }
+}
