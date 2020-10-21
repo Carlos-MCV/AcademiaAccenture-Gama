289bb7bd4bf45ca81fa955e961c6a86a2c5a0f45
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-21 19:04:22 -0300

    Create Estagiario.java

---
+++ exercicio/empregados/Estagiario.java
@@ -0,0 +1,16 @@
+package exercicio.empregados;
+import exercicio.Empregado;
+public class Estagiario  extends Empregado {
+    private String nome;
+
+    public Estagiario(){
+        this.nome="Leonardo";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+    }
+}
