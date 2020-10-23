3fe0274c4a1f457025581a57286713dbc4db2ae9
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-23 15:07:35 -0300

    Create Ex05.java

---
+++ ex05/Ex05.java
@@ -0,0 +1,20 @@
+package ex05;
+
+public class Ex05 {
+    public static void main(String[] args) {
+        House newHouse=new House();
+
+        newHouse.abrirPorta1();
+        newHouse.abrirPorta2();
+        newHouse.fecharPorta2();
+        newHouse.abrirPorta3();
+        newHouse.pintarCasa();
+        newHouse.howManyDoorsAreOpen();
+
+
+
+
+
+    }
+
+}
