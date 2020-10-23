f137cad991bc1eb0ff86f32f6de32838f0618e31
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-23 18:34:07 -0300

    Update Account.java

--- ex02/Account.java
+++ ex02/Account.java
@@ -1,11 +1,42 @@
 package ex02;
 
+import java.util.Scanner;
+
 public class Account {
+    Scanner teclado= new Scanner(System.in);
+    private Account account;
+    private SavingsAccount savingsAccount;
     private double saldo;
+    private String type;
 
 
     public Account() {
-        this.setSaldo(300);
+    }
+
+
+
+    public String getType() {
+        return "Account";
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+        //return saldo;
+    }
+
+
+
+    public void situacao(){
+        System.out.println("Dados da Conta: ");
+        System.out.print("Saldo: "+this.getSaldo());
     }
 
     public void deposit(double deposito){
@@ -21,17 +52,17 @@ public class Account {
         return this.getSaldo();
     }
 
-    public double getSaldo() {
-        return saldo;
+    public void transfer(Account a,SavingsAccount s){
+        this.account=a;
+        this.savingsAccount=s;
+
+        System.out.println("Quanto você quer transferir para a poupança?");
+        int transferAmount=teclado.nextInt();
+
+        account.setSaldo(account.getSaldo()-transferAmount);
+        savingsAccount.setSaldo(savingsAccount.getSaldo()+transferAmount);
+        System.out.println("Agora você tem: "+savingsAccount.getSaldo()+" na poupança!!");
+
     }
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-        //return saldo;
-    }
-
-    public void situacao(){
-        System.out.println("Dados da Conta: ");
-        System.out.print("Saldo: "+this.getSaldo());
-    }
 }
