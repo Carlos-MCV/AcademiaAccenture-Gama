3ab7d6d40cf4fa5a51366c1dff64a7699c6f857e
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-23 18:34:40 -0300

    Update Ex02.java

--- ex02/Ex02.java
+++ ex02/Ex02.java
@@ -4,12 +4,14 @@ public class Ex02 {
     public static void main(String[] args) {
         Account conta00=new Account();
 
+        CheckingAccount checkingNew=new CheckingAccount();
 
+        SavingsAccount savingsNew=new SavingsAccount();
 
         conta00.deposit(40);
         conta00.withdraw(10);
+        conta00.transfer(conta00,savingsNew);
         conta00.accountIncome();
-        
         conta00.situacao();
 
     }
