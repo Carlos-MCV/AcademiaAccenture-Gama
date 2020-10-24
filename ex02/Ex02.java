bc33c1d3abe157b42c562bb7e4f34781792b1613
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-23 22:09:08 -0300

    Update Ex02.java

--- ex02/Ex02.java
+++ ex02/Ex02.java
@@ -2,17 +2,21 @@ package ex02;
 
 public class Ex02 {
     public static void main(String[] args) {
-        Account conta00=new Account();
 
         CheckingAccount checkingNew=new CheckingAccount();
 
         SavingsAccount savingsNew=new SavingsAccount();
 
-        conta00.deposit(40);
-        conta00.withdraw(10);
-        conta00.transfer(conta00,savingsNew);
-        conta00.accountIncome();
-        conta00.situacao();
+        LifeInsurance insurancenew=new LifeInsurance();
+
+        savingsNew.deposit(40);
+        savingsNew.withdraw(10);
+        savingsNew.transfer(savingsNew);
+        savingsNew.accountIncome();
+        savingsNew.situacao();
+
+        checkingNew.deposit(100);
+        checkingNew.mostrarTaxa();
 
     }
 }
