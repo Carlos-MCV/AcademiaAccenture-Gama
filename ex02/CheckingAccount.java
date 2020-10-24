750af6038cd62ece810f7b67203907be91684a91
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-23 22:08:59 -0300

    Update CheckingAccount.java

--- ex02/CheckingAccount.java
+++ ex02/CheckingAccount.java
@@ -1,9 +1,14 @@
 package ex02;
 
-public class CheckingAccount extends Account{
+public class CheckingAccount extends Account implements TaxableI{
 
     public String getType() {
         return "Checking Account";
+    }
+
+
+    public void mostrarTaxa(){
+        System.out.println("Taxa a ser paga..."+this.getTaxes());
     }
 
     @Override
@@ -16,5 +21,15 @@ public class CheckingAccount extends Account{
         super.withdraw(saque);
         this.setSaldo(getSaldo()-saque);
         this.setSaldo(getSaldo()+0.10);
+    }
+
+    @Override
+    public double getTaxes() {
+        this.setTaxes(getSaldo()*1/100);
+        return this.taxes;
+    }
+
+    public void setTaxes(double taxes){
+        this.taxes=taxes;
     }
 }
