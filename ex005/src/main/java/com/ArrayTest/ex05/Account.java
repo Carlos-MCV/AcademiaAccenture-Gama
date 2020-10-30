39841da41cc3acbf91d0792b0f7a0ed7c6b5ed1b
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-10-30 18:47:16 -0300

    Create Account.java

---
+++ ex005/src/main/java/com/ArrayTest/ex05/Account.java
@@ -0,0 +1,28 @@
+package main.java.com.ArrayTest.ex05;
+
+public class Account {
+    private double finalBalance;
+
+
+    public Account(double balance) {
+       this.finalBalance = 100;
+    }
+
+   public Account() {
+
+    }
+
+    public double getBalance() {
+        return finalBalance;
+    }
+
+    public void setBalance(double balance) {
+        this.finalBalance = balance;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Account's balance= " + finalBalance;
+    }
+}
