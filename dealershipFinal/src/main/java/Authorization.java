e6d705e988fd95fb14ddd6dedc5288888d14e508
Carlos-MCV <66149594+Carlos-MCV@users.noreply.github.com>
2020-11-04 18:34:21 -0300

    Create Authorization.java

---
+++ dealershipFinal/src/main/java/Authorization.java
@@ -0,0 +1,17 @@
+public class Authorization {
+    private Manager authorization;
+    private boolean autorized;
+
+
+    public void autorize(){
+    }
+
+
+    public boolean isAutorized() {
+        return autorized;
+    }
+
+    public void setAutorized(boolean autorized) {
+        this.autorized = autorized;
+    }
+}
